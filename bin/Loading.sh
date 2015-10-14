18ce95896f7eab56b5fff7f09f0d3120a1f792d9
Renan Cunha <renan.cunha20@gmail.com>
2015-10-14 15:24:18 -0300

    Create Loading.sh
    tela de loading, sem utilidade mas é divertido.

---
+++ bin/Loading.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+while true
+do
+clear
+echo "carregando"
+sleep 1
+clear
+echo "carregando ."
+sleep 1
+clear
+echo "carregando .."
+sleep 1
+clear
+echo "carregando ..."
+sleep 1
+done
+
+#tela de carregando, sem utilidade mas é divertido.
