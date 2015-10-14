059e072dff14b377ad315a90bd8c799d579cb967
Renan Cunha <renan.cunha20@gmail.com>
2015-10-14 10:15:27 -0300

    Create topForUnixAix.sh
    Unix AIX does not have "top" command to see processes on real time, so I did the little script to make a similar thing.

---
+++ topForUnixAix.sh
@@ -0,0 +1,10 @@
+#!/bin/ksh
+
+while true
+do
+clear
+ps -ef
+sleep 2
+done
+
+#Unix AIX does not have "top" command to see processes on real time, so I did the little script to make a similar thing.
