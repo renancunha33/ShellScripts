9698a5fc4eb4c526e35d18c00fb5b7690204abcc
Renan Cunha <renan.cunha20@gmail.com>
2015-10-23 11:23:43 -0200

    Create basicSystemInfo.sh

---
+++ basicSystemInfo.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+clear;echo "sistema: ";cat /etc/*-release;echo "Kernel: ";uname -a;echo "nome do host: ";hostname;echo "rede: ";ifconfig
+
+#Informaçoes basicas do SO. para inicio de uma analise de suporte.
